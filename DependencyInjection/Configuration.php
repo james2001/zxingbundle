59f224c202619dc9709bc2694269ec147e879f28
james2001 <stephane_rathgeber@hotmail.com>
2015-08-18 10:31:58 +0200

    fix unuse property

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('james2001_zxing');
+        $treeBuilder->root('james2001_zxing');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
